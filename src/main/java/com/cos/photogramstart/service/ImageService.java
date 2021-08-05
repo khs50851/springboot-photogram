1df407fbc2bb0554b41f6e423f3f2ccda5d3fd36
HyeokSeong Kwon <khs50851@naver.com>
2021-08-05 20:13:21 +0900

    image 경로 db에 저장하기

--- src/main/java/com/cos/photogramstart/service/ImageService.java
+++ src/main/java/com/cos/photogramstart/service/ImageService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.cos.photogramstart.config.auth.PrincipalDetails;
+import com.cos.photogramstart.domain.image.Image;
 import com.cos.photogramstart.domain.image.ImageRepository;
 import com.cos.photogramstart.web.dto.image.ImageUploadDto;
 
@@ -28,19 +29,16 @@ public class ImageService {
 		
 		UUID uuid = UUID.randomUUID(); // 랜덤으로 uuid 생성
 		String imageFileName = uuid+"_"+imageUploadDto.getFile().getOriginalFilename(); // 실제 파일명 ex) apple.jpg
-		System.out.println("이미지 파일 이름 : "+imageFileName);
-		
 		Path imageFilePath = Paths.get(uploadFoler+imageFileName); //get안에는 실제 저장될 장소+파일명이 들어가야함 yml에 파일 경로 적어놓음 C:/workspace/springbootwork/upload/
-		
-		System.out.println("경로 : "+uploadFoler);
-		System.out.println("imageFileName : "+imageFilePath);
-		
-		
 		try {
 			Files.write(imageFilePath, imageUploadDto.getFile().getBytes()); // 경로 안에 오리지널 파일을 바이트화 해서 넣음
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
+		
+		// image 테이블에 저장
+		Image image = imageUploadDto.toEntity(imageFileName,principalDetails.getUser());
+		Image imageEntity = imageRepository.save(image);
 		
 	}
 	
--- src/main/java/com/cos/photogramstart/web/dto/image/ImageUploadDto.java
+++ src/main/java/com/cos/photogramstart/web/dto/image/ImageUploadDto.java
@@ -2,6 +2,9 @@ package com.cos.photogramstart.web.dto.image;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import com.cos.photogramstart.domain.image.Image;
+import com.cos.photogramstart.domain.user.User;
+
 import lombok.Data;
 
 @Data
@@ -9,6 +12,15 @@ public class ImageUploadDto {
 	
 	private MultipartFile file; // 파일 받기
 	private String caption;
+	
+	public Image toEntity(String postImageUrl,User user) {
+		return Image.builder()
+				.caption(caption)
+				.postImageUrl(postImageUrl)
+				.user(user)
+				.build();
+				
+	}
 	
 	
 }
