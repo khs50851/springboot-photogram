97858e906ec85ef46fcd19422a0e0cbe2f9fea06
HyeokSeong Kwon <khs50851@naver.com>
2021-08-19 22:36:24 +0900

    테스트

--- src/main/java/com/cos/photogramstart/PhotogramStartApplication.java
+++ src/main/java/com/cos/photogramstart/PhotogramStartApplication.java
@@ -2,10 +2,17 @@ package com.cos.photogramstart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PhotogramStartApplication {
-
+public class PhotogramStartApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(PhotogramStartApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PhotogramStartApplication.class, args);
 	}
